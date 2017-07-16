53b27e7192818426f68bffbf82420a6dd1e0c665
Alkagar <alkagar@gmail.com>
2017-07-16 08:43:05 +0200

    feature
    add checking dependencies for single validator rule in parallel

--- src/rules/rules/is-max-length.js
+++ src/rules/rules/is-max-length.js
@@ -1,33 +1,49 @@
-const _ = require('lodash');
+const async = require('async');
+
 const isString = require('./is-string.js');
 
-module.exports = (options, callback) => {
-  const { schema, data, config, fieldName } = options;
+const isMaxLength = (options, callback) => {
+  const { data, config, fieldName } = options;
+  const notPassing = {
+    check: false,
+    message: 'error.validate.not-max',
+    config: config.v,
+    fieldName,
+  };
 
-  if(isString(options, () => {
-    if(err) {
-
+  try {
+    const length = data.length;
+    if (length > config.v) {
+      return callback(null, notPassing);
     }
-  }));
-
-  if (!_.isString(data)) {
-    return callback(null, {
-      check: false,
-      message: 'error.validate.not-string',
-      fieldName,
-    });
+  } catch (ex) {
+    return callback(null, notPassing);
   }
 
-  const length = data.length;
-  if (length > config.v) {
-    return callback(null, {
-      check: false,
-      message: 'error.validate.not-max',
-      config: config.v,
-      fieldName,
-    });
-  }
   return callback(null, {
     check: true,
   });
+};
+
+/**
+ * Checks if provided field is correct email address.
+ * @param {object} options
+ * @param {object} options.schema - full chema to validate
+ * @param {object} options.data - field value which validator should validate
+ * @param {object} options.fieldName - name of the field passed to validate
+ * @param {object} options.context - context of the field containing rest of the fields
+ * @param {object} options.config - object with validator configuration
+ * @param {function} finalCallback - callback which will return result of validation or error
+ * @returns {undefined} function does not return value, result is returned via callback provided
+ */
+module.exports = (options, finalCallback) => {
+  async.parallel(
+    [callback => isString(options, callback), callback => isMaxLength(options, callback)],
+    (err, validationResults) => {
+      if (err) {
+        return finalCallback(err, null);
+      }
+      return finalCallback(null, validationResults);
+    }
+  );
 };
--- src/rules/rules/is-string.js
+++ src/rules/rules/is-string.js
@@ -1,13 +1,29 @@
-const _ = require('lodash');
+const _isString = require('lodash.isstring');
 
-module.exports = (options, callback) => {
+const isString = (options, finalCallback) => {
   const { data, fieldName } = options;
 
-  if (!_.isString(data)) {
-    return callback(null, {
+  if (!_isString(data)) {
+    return finalCallback(null, {
       check: false,
       message: 'error.validate.not-string',
       fieldName,
     });
   }
+  return finalCallback(null, {
+    check: true,
+  });
 };
+
+/**
+ * Checks if provided field is correct email address.
+ * @param {object} options
+ * @param {object} options.schema - full chema to validate
+ * @param {object} options.data - field value which validator should validate
+ * @param {object} options.fieldName - name of the field passed to validate
+ * @param {object} options.context - context of the field containing rest of the fields
+ * @param {object} options.config - object with validator configuration
+ * @param {function} finalCallback - callback which will return result of validation or error
+ * @returns {undefined} function does not return value, result is returned via callback provided
+ */
+module.exports = isString;
